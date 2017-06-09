4ac4bb96fa8bdb2b22d8efe195cd8be1528dc02a
uuinnk <uuinnk@developmentontheedge.com>
2017-06-09 18:43:42 +0700

    fix TestOperation for int type

--- src/main/java/com/developmentontheedge/testbe5app/TestOperation.java
+++ src/main/java/com/developmentontheedge/testbe5app/TestOperation.java
@@ -15,8 +15,8 @@ public class TestOperation extends OperationSupport implements Operation
     {
         dps.add(new DynamicProperty("name", "Name", String.class,
                 presetValues.getOrDefault("name", "")));
-        dps.add(new DynamicProperty("value", "Value", String.class,
-                presetValues.getOrDefault("value", "")));
+        dps.add(new DynamicProperty("value", "Value", Integer.class,
+                Integer.parseInt(presetValues.getOrDefault("value", "1"))));
 
         return dps;
     }
