2dfc958de1c762c0c1ed1a102374d4c8d78965d1
QProgS <uu.innk@gmail.com>
2017-07-22 23:13:03 +0700

    update frontend

--- src/frontend/scripts/initApp.js
+++ src/frontend/scripts/initApp.js
@@ -3,7 +3,10 @@ import React    from 'react';
 import {be5} from 'be5-react';
 import TestBe5app      from './components/testBe5app';
 import TestAction      from './actions/test';
-import '../sass/styles.scss'
+
+import 'react-datetime/css/react-datetime.css';
+import 'react-select/dist/react-select.css';
+import '../sass/styles.scss';
 
 
 be5.registerAction('test', TestAction);
