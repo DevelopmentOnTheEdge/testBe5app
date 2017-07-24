32216fd3c36de1bf831af85fd6730613c98601b2
uuinnk <uuinnk@developmentontheedge.com>
2017-07-24 18:50:16 +0700

    add DEFAULT_VALUE

--- src/groovy/operations/TestOperation.groovy
+++ src/groovy/operations/TestOperation.groovy
@@ -1,7 +1,10 @@
+package operations
+
 import com.developmentontheedge.be5.operation.Operation
 import com.developmentontheedge.be5.operation.OperationContext
+import com.developmentontheedge.be5.operation.OperationResult
 import com.developmentontheedge.be5.operation.OperationSupport
-
+import com.developmentontheedge.be5.util.DateUtils
 
 
 class TestOperation extends OperationSupport implements Operation
@@ -10,25 +13,32 @@ class TestOperation extends OperationSupport implements Operation
     @Override
     Object getParameters(Map<String, Object> presetValues) throws Exception
     {
-        dps << [name: "name", DISPLAY_NAME: "Имя"]
+        dps << [
+                name        : "name",
+                DISPLAY_NAME: "Имя",
+                DEFAULT_VALUE: "Test"
+        ]
 
         dps << [
-                name        : "beginDate",
-                DISPLAY_NAME: "Дата начала",
-                TYPE        : java.sql.Date
+                name         : "beginDate",
+                DISPLAY_NAME : "Дата начала",
+                TYPE         : java.sql.Date,
+                DEFAULT_VALUE: DateUtils.curMonthBegin()
         ]
 
         dps << [
                 name         : "reason",
                 DISPLAY_NAME : "Причина снятия предыдущего работника",
-                TAG_LIST_ATTR: [["fired", "Уволен"], ["vacation", "Отпуск"], ["sick", "На больничном"], ["other", "Иная причина"]] as String[][]
+                TAG_LIST_ATTR: [["fired", "Уволен"], ["vacation", "Отпуск"], ["sick", "На больничном"], ["other", "Иная причина"]] as String[][],
+                DEFAULT_VALUE: "vacation"
         ]
 
         dps << [
                 name                   : "reasonMulti",
                 DISPLAY_NAME           : "Множественный выбор",
                 MULTIPLE_SELECTION_LIST: true,
-                TAG_LIST_ATTR          : [["fired", "Уволен"], ["vacation", "Отпуск"], ["sick", "На больничном"], ["other", "Иная причина"]] as String[][]
+                TAG_LIST_ATTR          : [["fired", "Уволен"], ["vacation", "Отпуск"], ["sick", "На больничном"], ["other", "Иная причина"]] as String[][],
+                DEFAULT_VALUE          : ["vacation","sick"] as String[]
         ]
 
         return dps
@@ -37,8 +47,7 @@ class TestOperation extends OperationSupport implements Operation
     @Override
     void invoke(Object parameters, OperationContext context) throws Exception
     {
-        //String sql = generateSql( connector, false );
-        //db.insert(sql);
+        setResult(OperationResult.finished("hello!"))
     }
 
 }
