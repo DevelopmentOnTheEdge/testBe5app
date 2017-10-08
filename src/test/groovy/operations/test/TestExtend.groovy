c7d23235ff403a8d16c4846fef6ac14386c6c896
Innokentiy <uu.innk@gmail.com>
2017-10-09 01:45:23 +0700

    TestExtendGroovy - dev reload in process

--- src/test/groovy/operations/test/TestExtend.groovy
+++ src/test/groovy/operations/test/TestExtend.groovy
@@ -1,6 +1,8 @@
 package operations.test
 
 import com.developmentontheedge.be5.api.Request
+import com.developmentontheedge.be5.api.services.ProjectProvider
+import com.developmentontheedge.be5.env.Inject
 import com.developmentontheedge.be5.model.FormPresentation
 import com.developmentontheedge.be5.test.SqlMockOperationTest
 import org.junit.Test
@@ -9,14 +11,18 @@ import static org.junit.Assert.assertEquals
 
 class TestExtend extends SqlMockOperationTest
 {
+    @Inject ProjectProvider projectProvider
     @Test
     void testGet()
     {
-        Request req = getSpyMockRecForOp("_system_", "Session variables", "SessionVariablesEdit", "remoteAddr",
+        projectProvider.reloadProject()
+
+        Request req = getSpyMockRecForOp("testtable", "Test 1D", "TestExtendGroovy", "remoteAddr",
                 "", ["remoteAddr":"199.168.0.1"])
+
         FormPresentation first = operationService.generate(req).getFirst()
 
-//        assertEquals("{'values':{'test':''},'meta':{'/test':{'displayName':'Test'}},'order':['/test']}",
+//        assertEquals("{'values':{'newValue':'199.168.0.1'},'meta':{'/newValue':{'displayName':'Новое значение:'}},'order':['/newValue']}",
 //                oneQuotes(first.getBean().toString()))
     }
 
