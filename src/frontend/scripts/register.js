d11fa5b8e7255fcd01d7aa2ad869a3e89b49a7b2
uuinnk <uuinnk@developmentontheedge.com>
2018-03-01 19:47:01 +0700

    fix dependencies

--- src/frontend/scripts/register.js
+++ src/frontend/scripts/register.js
@@ -4,15 +4,15 @@ import 'react-virtualized/styles.css';
 import 'react-virtualized-select/styles.css';
 import 'react-s-alert/dist/s-alert-default.css';
 
-import 'be5-react/src/sass/styles.scss';
+import 'beanexplorer-react/src/propertySet.css';
 
+import 'be5-react/src/sass/styles.scss';
 import 'be5-react/src/css/base.css';
 import 'be5-react/src/css/document.css';
 import 'be5-react/src/css/splitPane.css';
 import 'be5-react/src/css/form.css';
 import 'be5-react/src/css/table.css';
 import 'be5-react/src/css/formWizard.css';
-import 'be5-react/src/css/propertySet.css';
 import 'be5-react/src/css/errorPane.css';
 import 'be5-react/src/css/qBuilder.css';
 import 'be5-react/src/css/roleSelector.css';
