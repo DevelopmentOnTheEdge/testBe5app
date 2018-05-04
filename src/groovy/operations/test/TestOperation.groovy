2e46daddd17eafd1d4de15069dc4b8a4daeb3a8e
uuinnk <uuinnk@developmentontheedge.com>
2018-05-04 16:57:43 +0700

    move to support package

--- src/groovy/operations/test/TestOperation.groovy
+++ src/groovy/operations/test/TestOperation.groovy
@@ -1,6 +1,6 @@
 package test
 
-import com.developmentontheedge.be5.operation.GOperationSupport
+import com.developmentontheedge.be5.operation.support.GOperationSupport
 import com.developmentontheedge.be5.operation.Operation
 import com.developmentontheedge.be5.operation.OperationResult
 
