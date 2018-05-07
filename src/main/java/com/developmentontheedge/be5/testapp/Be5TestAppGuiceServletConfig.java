4bae1e285a26d52c63590974365a2c532ccf47b4
Innokentiy Krivonosov <uu.innk@gmail.com>
2018-05-08 01:36:36 +0700

    add ControllerSupport, config Controllers

---
+++ src/main/java/com/developmentontheedge/be5/testapp/Be5TestAppGuiceServletConfig.java
@@ -0,0 +1,22 @@
+package com.developmentontheedge.be5.testapp;
+
+import com.developmentontheedge.be5.ServerModule;
+import com.developmentontheedge.be5.modules.core.CoreModule;
+import com.developmentontheedge.be5.servlet.TemplateModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+
+public class Be5TestAppGuiceServletConfig extends GuiceServletContextListener
+{
+    @Override
+    protected Injector getInjector()
+    {
+        return Guice.createInjector(
+                new ServerModule(),
+                new CoreModule(),
+                new TemplateModule()
+        );
+    }
+}
