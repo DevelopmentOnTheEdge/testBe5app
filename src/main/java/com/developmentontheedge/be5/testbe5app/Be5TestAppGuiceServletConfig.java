ee7810a06a16122e80f277c640b9c3e1ac8b9ac1
uuinnk <uuinnk@developmentontheedge.com>
2018-05-24 17:43:55 +0700

    update config

--- src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppGuiceServletConfig.java
+++ src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppGuiceServletConfig.java
@@ -2,13 +2,13 @@ package com.developmentontheedge.be5.testbe5app;
 
 import com.developmentontheedge.be5.ServerModule;
 import com.developmentontheedge.be5.modules.core.CoreModule;
+import com.developmentontheedge.be5.servlet.Be5GuiceServletContextListener;
 import com.developmentontheedge.be5.servlet.TemplateModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceServletContextListener;
 
 
-public class Be5TestAppGuiceServletConfig extends GuiceServletContextListener
+public class Be5TestAppGuiceServletConfig extends Be5GuiceServletContextListener
 {
     @Override
     protected Injector getInjector()
