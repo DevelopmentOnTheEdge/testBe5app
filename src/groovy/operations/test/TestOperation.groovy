00eeeb09f9f5f97482660be0badcf41962ac0d0f
uuinnk <uuinnk@developmentontheedge.com>
2018-05-29 19:02:32 +0700

    move operations and queries to server

--- src/groovy/operations/test/TestOperation.groovy
+++ src/groovy/operations/test/TestOperation.groovy
@@ -1,6 +1,6 @@
 package test
 
-import com.developmentontheedge.be5.operations.support.GOperationSupport
+import com.developmentontheedge.be5.server.operations.support.GOperationSupport
 import com.developmentontheedge.be5.operation.model.Operation
 import com.developmentontheedge.be5.operation.model.OperationResult
 import com.developmentontheedge.be5.base.util.DpsUtils
--- src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppGuiceServletConfig.java
+++ src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppGuiceServletConfig.java
@@ -1,9 +1,9 @@
 package com.developmentontheedge.be5.testbe5app;
 
-import com.developmentontheedge.be5.ServerModule;
+import com.developmentontheedge.be5.server.ServerModule;
 import com.developmentontheedge.be5.modules.core.CoreModule;
-import com.developmentontheedge.be5.servlet.Be5GuiceServletContextListener;
-import com.developmentontheedge.be5.servlet.TemplateModule;
+import com.developmentontheedge.be5.server.servlet.Be5GuiceServletContextListener;
+import com.developmentontheedge.be5.server.servlet.TemplateModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
