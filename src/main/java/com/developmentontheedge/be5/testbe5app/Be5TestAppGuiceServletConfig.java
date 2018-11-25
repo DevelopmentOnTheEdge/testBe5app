33ca2e56fed14b1bd2963d1a9e65b57ba6e51907
Innokentiy <uu.innk@gmail.com>
2018-11-25 21:35:20 +0700

    add EncodePasswords for migrate, fixes #116

--- src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppGuiceServletConfig.java
+++ src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppGuiceServletConfig.java
@@ -1,11 +1,16 @@
 package com.developmentontheedge.be5.testbe5app;
 
 import com.developmentontheedge.be5.modules.core.CoreModule;
+import com.developmentontheedge.be5.modules.core.services.LoginService;
+import com.developmentontheedge.be5.modules.core.services.impl.CryptoLoginService;
 import com.developmentontheedge.be5.server.ScopedServerModule;
 import com.developmentontheedge.be5.server.servlet.Be5GuiceServletContextListener;
 import com.developmentontheedge.be5.server.servlet.TemplateModule;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Scopes;
+import com.google.inject.util.Modules;
 
 
 public class Be5TestAppGuiceServletConfig extends Be5GuiceServletContextListener
@@ -14,9 +19,20 @@ public class Be5TestAppGuiceServletConfig extends Be5GuiceServletContextListener
     protected Injector getInjector()
     {
         return Guice.createInjector(
-                new CoreModule(),
+                Modules.override(
+                        new CoreModule()
+                ).with(new Be5TestAppModule()),
                 new ScopedServerModule(),
                 new TemplateModule()
         );
+    }
+
+    private static class Be5TestAppModule extends AbstractModule
+    {
+        @Override
+        protected void configure()
+        {
+            bind(LoginService.class).to(CryptoLoginService.class).in(Scopes.SINGLETON);
+        }
     }
 }
