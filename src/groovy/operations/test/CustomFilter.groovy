6f0e2f5a88453a6de6c3587609124ced80730bb8
Innokentiy <uu.innk@gmail.com>
2018-12-06 00:00:13 +0700

    support multiple values for param

---
+++ src/groovy/operations/test/CustomFilter.groovy
@@ -0,0 +1,23 @@
+package test
+
+import com.developmentontheedge.be5.base.model.GDynamicPropertySetSupport
+import com.developmentontheedge.be5.databasemodel.util.DpsUtils
+import com.developmentontheedge.be5.server.operations.FilterOperation
+import com.developmentontheedge.beans.DynamicPropertySet
+
+class CustomFilter extends FilterOperation
+{
+    @Override
+    DynamicPropertySet getFilterParameters(Map<String, Object> presetValues) throws Exception
+    {
+        def params = new GDynamicPropertySetSupport()
+        params.add {
+            name = "ID"
+            TYPE = Integer
+            TAG_LIST_ATTR = queries.getTagsFromSelectionView(getInfo().getEntityName())
+            MULTIPLE_SELECTION_LIST = true
+            CAN_BE_NULL = true
+        }
+        return DpsUtils.setValues(params, presetValues)
+    }
+}
