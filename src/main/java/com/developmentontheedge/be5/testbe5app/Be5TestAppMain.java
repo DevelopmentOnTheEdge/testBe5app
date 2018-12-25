b8b4d38dc3fb6b9b17e2aa228de2c14ddf31a762
uuinnk <uuinnk@developmentontheedge.com>
2018-12-25 11:49:44 +0700

    rename to EmbeddedJetty, remove static runWebApp()

--- src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppMain.java
+++ src/main/java/com/developmentontheedge/be5/testbe5app/Be5TestAppMain.java
@@ -1,11 +1,11 @@
 package com.developmentontheedge.be5.testbe5app;
 
-import com.developmentontheedge.be5.jetty.EmbeddedJettyUtils;
+import com.developmentontheedge.be5.jetty.EmbeddedJetty;
 
 public class Be5TestAppMain
 {
     public static void main(String... args)
     {
-        new EmbeddedJettyUtils().run();
+        new EmbeddedJetty().run();
     }
 }
