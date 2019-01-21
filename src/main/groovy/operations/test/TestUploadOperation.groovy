ee0893b8746f8cc685809382bb2a6dcd6c22adbc
uuinnk <uuinnk@developmentontheedge.com>
2019-01-21 16:09:13 +0700

    add TestUploadOperation

---
+++ src/main/groovy/operations/test/TestUploadOperation.groovy
@@ -0,0 +1,33 @@
+package operations.test
+
+import com.developmentontheedge.be5.databasemodel.util.DpsUtils
+import com.developmentontheedge.be5.groovy.GDynamicPropertySetSupport
+import com.developmentontheedge.be5.operation.Operation
+import com.developmentontheedge.be5.server.operations.support.OperationSupport
+import com.developmentontheedge.be5.web.impl.FileUploadWrapper
+
+class TestUploadOperation extends OperationSupport implements Operation
+{
+    @Override
+    Object getParameters(Map<String, Object> presetValues) throws Exception
+    {
+        def params = new GDynamicPropertySetSupport()
+        params.add {
+            TYPE = File
+            name = "name"
+            DISPLAY_NAME = "Имя"
+        }
+
+        return DpsUtils.setValues(params, presetValues)
+    }
+
+    @Override
+    void invoke(Object parameters)
+    {
+        def uploadWrapper = (FileUploadWrapper) request.getRawRequest()
+        def item = uploadWrapper.getFileItem("name")
+        File writeFile = new File(System.getProperty("user.dir") + "/target/" + item.getName())
+        item.write( writeFile )
+        setResultFinished()
+    }
+}
