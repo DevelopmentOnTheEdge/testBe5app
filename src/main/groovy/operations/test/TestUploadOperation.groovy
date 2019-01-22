4d29659e37bf0e9a8583d682c3e5190a16edb423
uuinnk <uuinnk@developmentontheedge.com>
2019-01-22 19:18:41 +0700

    test setValuesWithFiles

--- src/main/groovy/operations/test/TestUploadOperation.groovy
+++ src/main/groovy/operations/test/TestUploadOperation.groovy
@@ -21,12 +21,53 @@ class TestUploadOperation extends GOperationSupport
     @Override
     void invoke(Object parameters)
     {
-        String[] files = params.getValue("files")
-        for (String file : files) {
+        String[] fileNames = params.getValue("files")
+        for (String file : fileNames) {
             def fileItem = getFileItem(file)
             File writeFile = new File(System.getProperty("user.dir") + "/target/" + fileItem.getName())
             fileItem.write(writeFile)
         }
+
+//        File[] files = (File[]) params.getValue("files")
+//        for (File file : files) {
+//            File writeFile = new File(System.getProperty("user.dir") + "/target/" + file.getName())
+//            Files.copy(file.toPath(), writeFile.toPath())
+//        }
+
         setResultFinished()
     }
+
+//    public <T extends DynamicPropertySet> T setValuesWithFiles(T dps, Map<String, ?> values)
+//    {
+//        DpsUtils.setValues(dps, values);
+//        for (DynamicProperty property : dps)
+//        {
+//            if (property.getType() == File.class && property.getValue() != null)
+//            {
+//                if (property.getValue() instanceof String[])
+//                {
+//                    String[] fileNames = (String[]) property.getValue();
+//                    File[] files = new File[fileNames.length];
+//                    for (int i = 0; i < fileNames.length; i++)
+//                    {
+//                        files[i] = ((DiskFileItem) getFileItem(fileNames[i])).getStoreLocation();
+//                    }
+//                    property.setValue(files);
+//                }
+//                else
+//                {
+//                    File file = ((DiskFileItem) getFileItem((String) property.getValue())).getStoreLocation();
+//                    if ((boolean) property.getAttribute(BeanInfoConstants.MULTIPLE_SELECTION_LIST))
+//                    {
+//                        property.setValue(new File[]{file});
+//                    }
+//                    else
+//                    {
+//                        property.setValue(file);
+//                    }
+//                }
+//            }
+//        }
+//        return dps;
+//    }
 }
