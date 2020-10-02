fc007b02e4a41ed43491de2b28f5e70c8d563c9b
mixa <mixa@developmentontheedge.com>
2020-10-02 13:31:33 +0700

    GROUP_INITIALLY_CLOSED property attribute is implemented

--- src/main/groovy/operations/test/TestGroupOperation.groovy
+++ src/main/groovy/operations/test/TestGroupOperation.groovy
@@ -6,14 +6,16 @@ import com.developmentontheedge.be5.server.operations.support.GOperationSupport
 import groovy.transform.TypeChecked
 
 @TypeChecked
-class TestGroupCollapsedOperation extends GOperationSupport implements Operation {
+class TestGroupOperation extends GOperationSupport implements Operation {
 
     @Override
     Object getParameters(Map<String, Object> presetValues) throws Exception
     {
-        params.add { TYPE = String; name = "prop1"; DISPLAY_NAME = "Property 1"; GROUP_ID = 1; GROUP_NAME = "Group 1"}
+        params.add { TYPE = String; name = "prop1"; DISPLAY_NAME = "Property 1"; GROUP_ID = 1; GROUP_NAME = "Group 1";
+            GROUP_INITIALLY_CLOSED = false }
         params.add { TYPE = String; name = "prop2"; DISPLAY_NAME = "Property 2"; GROUP_ID = 1; GROUP_NAME = "Group 1"}
-        params.add { TYPE = String; name = "prop3"; DISPLAY_NAME = "Property 3"; GROUP_ID = 2; GROUP_NAME = "Group 2"}
+        params.add { TYPE = String; name = "prop3"; DISPLAY_NAME = "Property 3"; GROUP_ID = 2; GROUP_NAME = "Group 2";
+            GROUP_INITIALLY_CLOSED = true }
         params.add { TYPE = String; name = "prop4"; DISPLAY_NAME = "Property 4"; GROUP_ID = 2; GROUP_NAME = "Group 2"}
         params.add { TYPE = String; name = "prop5"; DISPLAY_NAME = "Property 5"; GROUP_ID = 2; GROUP_NAME = "Group 2"}
         params.add { TYPE = String; name = "prop5"; DISPLAY_NAME = "Property 6"; GROUP_ID = 3; GROUP_NAME = "Group 3"}
